bc0a83935f857ee72cdaf08615c2ee9e276a225b
Теори Накубуру <nakuburu@yandex.ru>
2021-01-22 22:45:28 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v;
+            Console.WriteLine("введите число N");
+            int N = Convert.ToInt32(Console.ReadLine());
+            for (int a = 1 ; a < N ; ++a)
+            {
+                v = 2*1/a;
+                Console.WriteLine("2*1/" + a + "=" + v);
+            }
+            v = 2 * 1 / N;
+            Console.WriteLine("2*1/" + N + "=" + v);
+            Console.ReadKey();
+        }
+    }
+}
